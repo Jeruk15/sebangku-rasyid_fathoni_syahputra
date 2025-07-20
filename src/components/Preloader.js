8de011745c5ec295e2c09263efd539c479e4410c
Jeruk15 <creworange6@gmail.com>
2025-07-20 18:38:09 +0700

    test

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Image } from '@themesberg/react-bootstrap';
+import { Image, Spinner } from '@themesberg/react-bootstrap';
 
 import ReactLogo from "../assets/img/technologies/react-logo.svg";
 
@@ -15,6 +15,17 @@ export default (props) => {
         src={ReactLogo} 
         height={40}
         alt="Loading..." 
+        onError={(e) => {
+          // Fallback to spinner if image fails to load
+          e.target.style.display = 'none';
+          e.target.nextSibling.style.display = 'block';
+        }}
+      />
+      <Spinner 
+        animation="border" 
+        variant="primary" 
+        style={{ display: 'none' }}
+        className="loader-element"
       />
     </div>
   );
